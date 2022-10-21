5aca364b30d303edaca3220e340062087010c904
visalatas <94117032+visalatas@users.noreply.github.com>
2022-10-21 16:55:29 +0300

    .

--- CandidateCore/Repositories/IRepository.cs
+++ CandidateCore/Repositories/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CandidateCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,15 @@ using System.Threading.Tasks;
 
 namespace CandidateCore.Repository
 {
-    public interface IRepository<TPrimaryKey>
+    //TEntity tipi BaseEntity veya baseentity den türemiş sınıflar
+    public interface IRepository<TEntity, TPrimaryKey> where TEntity : BaseEntity<TPrimaryKey>
     {
         //Temel veri tabanı işlemlerini içeren temel depo
+
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> GetByIdAsync(TPrimaryKey id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task UpdateAsync(TEntity entity);
+        Task RomoveAsync(TEntity entity);
     }
 }
---
+++ CandidateRepository/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using CandidateCore.Models;
+using CandidateCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateRepository.Repositories
+{
+    public class Repository<TEntity, TPrimaryKey> : IRepository<TEntity, TPrimaryKey> where TEntity : BaseEntity<TPrimaryKey>
+    {
+        protected readonly CondidateDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(CondidateDbContext context, DbSet<TEntity> dbSet)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>()
+        }
+
+        public Task<TEntity> AddAsync(TEntity entity)
+        {
+            
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+        }
+
+        public Task<TEntity> GetByIdAsync(TPrimaryKey id)
+        {
+        }
+
+        public Task RomoveAsync(TEntity entity)
+        {
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+        }
+    }
+}
